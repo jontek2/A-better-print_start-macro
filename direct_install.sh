5b13725e8dbb4879ad84f26e03968343c8f484a2
ss1gohan13 <89548129+ss1gohan13@users.noreply.github.com>
2025-02-18 23:15:49 -0600

    Create direct_install.sh

---
+++ direct_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Direct installation script for START_PRINT macro
+# Created: 2025-02-19 05:09:59 UTC
+# Author: ss1gohan13
+
+# Get the raw content directly from GitHub and pipe it to bash
+curl -sSL https://raw.githubusercontent.com/ss1gohan13/A-better-print_start-macro-SV08/main/install_start_print.sh | bash
